0ddd41e7c43f81ea26b2713e9a08ce6f54b79a6b
Neji <nejis@ebos.com.cy>
2024-12-02 19:55:43 +0100

    secure username and password for DW

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,18 @@
-// Home.js
 import React from 'react';
 import { FaTools, FaCogs, FaChartBar } from 'react-icons/fa';
 import Card from '../components/cards';
 
-
 const Home = () => {
+  const username = process.env.REACT_APP_USERNAME;
+  const password = process.env.REACT_APP_PASSWORD;
+
+  const decisionWorkflowLink = `https://${username}:${password}@probono.irtsysx.fr/`;
+
   return (
     <div className="home-container">
       <div className="cards-container">
         <Card icon={FaTools} title="Solution Catalog" link="/tools" />
-        <Card icon={FaCogs} title="Decision Workflow" link="/decision-support" />
+        <Card icon={FaCogs} title="Decision Workflow" link={decisionWorkflowLink} />
         <Card icon={FaChartBar} title="Data Visualization" link="/data-visualizations" />
       </div>
     </div>
