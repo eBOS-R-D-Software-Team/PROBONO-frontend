a23c8a04ec8ef3829fc6d897ef42b2338c9c8b29
Neji <nejis@ebos.com.cy>
2026-06-11 11:08:45 +0200

    add api registry redirection

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import { FaHome, FaChartBar, FaAngleLeft, FaAngleRight, FaTools, FaCogs, FaFileAlt, FaSearch } from 'react-icons/fa';
-
+import { FaHome, FaChartBar, FaAngleLeft, FaAngleRight, FaTools, FaCogs, FaFileAlt, FaSearch, FaPlug } from 'react-icons/fa';
 const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const location = useLocation();
-
   const decisionWorkflowLink = `https://gbn-management.cds-probono.eu/?automatic_keycloak_login=true`;
   const solutionFinderLink = `https://solution-finder.cds-probono.eu`;
-
+  const apiregistry = `https://data-platform.cds-probono.eu/api-registry/`;
   const toggleSidebar = () => setIsCollapsed(!isCollapsed);
   const isGbnActive = location.pathname.includes('gbn');
-
   return (
     <div id="left-sidebar" className={isCollapsed ? 'collapsed' : ''}>
       <button onClick={toggleSidebar} className="collapse-btn">
@@ -19,7 +16,6 @@ const Sidebar = () => {
       </button>
       <div className="sidebar-nav">
         <ul className="custom-sidebar-list">
-
           {/* 1. Home */}
           <li>
             <NavLink to="/" end>
@@ -27,7 +23,6 @@ const Sidebar = () => {
               {!isCollapsed && <span>Home</span>}
             </NavLink>
           </li>
-
           {/* 2. Solution Finder (External) */}
           <li>
             <a href={solutionFinderLink} target="_blank" rel="noopener noreferrer">
@@ -35,7 +30,6 @@ const Sidebar = () => {
               {!isCollapsed && <span>Solution Finder</span>}
             </a>
           </li>
-
           {/* 3. Solutions Catalogue */}
           <li>
             <NavLink to="/tools">
@@ -43,7 +37,6 @@ const Sidebar = () => {
               {!isCollapsed && <span>Solutions Catalogue</span>}
             </NavLink>
           </li>
-
           {/* 4. GBN Management (External) */}
           <li>
             <a href={decisionWorkflowLink} className={isGbnActive ? 'active' : ''}>
@@ -51,7 +44,6 @@ const Sidebar = () => {
               {!isCollapsed && <span>GBN management</span>}
             </a>
           </li>
-
           {/* 5. Data Visualizations */}
           <li>
             <NavLink to="/labs">
@@ -59,7 +51,6 @@ const Sidebar = () => {
               {!isCollapsed && <span>Data Visualizations</span>}
             </NavLink>
           </li>
-
           {/* 6. Tools Description */}
           <li>
             <NavLink to="/tools-descriptions">
@@ -67,11 +58,16 @@ const Sidebar = () => {
               {!isCollapsed && <span>Tools Description</span>}
             </NavLink>
           </li>
-
+          {/* 7. API Registry (External) */}
+          <li>
+            <a href={apiregistry} target="_blank" rel="noopener noreferrer">
+              <FaPlug />
+              {!isCollapsed && <span>API Registry</span>}
+            </a>
+          </li>
         </ul>
       </div>
     </div>
   );
 };
-
 export default Sidebar;
